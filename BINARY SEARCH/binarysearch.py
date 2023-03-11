7b9fd6c7531fcd786a8d7f4e7728376aa33cf7e0
AV55CS <121622662+AV55CS@users.noreply.github.com>
2023-03-11 16:16:48 +0530

    Update binarysearch.py

--- BINARY SEARCH/binarysearch.py
+++ BINARY SEARCH/binarysearch.py
@@ -1,6 +1,4 @@
-class Solution:
-   
-    '''
+ '''
 approach  using boundary index  left and right
 Binary search
 input:sorted array(num) and a target value (target)to be searched
@@ -9,15 +7,22 @@ output:index of the target value
 
 
     def search(self,num, target ) :
-        left = 0  # lowerbound
-        right = len(num)-1  # upperbound
-        while (left <= right):  # while the index of list in range
+      # lowerbound
+        left = 0  
+        # upperbound
+        right = len(num)-1  
+         # while the index of list in range
+        while (left <= right): 
             mid = (left + right) // 2
-            if num[mid] == target:  # middle element is the target value
+            # middle element is the target value
+            if num[mid] == target: 
                 return mid
-            elif num[mid] > target: # if middle element is greater then cut rightmost part
+             # if middle element is greater then cut rightmost part
+            elif num[mid] > target: 
                 right = mid - 1
-            else:  # if middle element is on right then cut the left most part
+             # if middle element is on right then cut the left most part
+            else: 
                 left = mid + 1
-
-        return -1  # if we don't find the target value
+                
+         # if we don't find the target value
+        return -1 
