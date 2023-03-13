542f09d4d72421db2c6bb4e569cb3ed2840bb21c
AV55CS <121622662+AV55CS@users.noreply.github.com>
2023-03-13 11:54:33 +0530

    Update StartingandEndingPos.py
    finding the first and last position using generic binary search .

--- LearningwithPython/ProblemsonBinarysearch/StartingandEndingPos.py
+++ LearningwithPython/ProblemsonBinarysearch/StartingandEndingPos.py
@@ -1,1 +1,40 @@
+'''
+Given an array of integers nums sorted in ascending order, find the starting and ending position of a given number.
+ nums:    sorted list of numbers(reapeating values are allowed)
+ target : the value whose position to be searched 
+ Note: we have written separate condition function for both the functions in order to use our generic binary search function
 
+'''
+def first_position(nums, target):
+    def condition(mid):
+        if nums[mid] == target:
+            if mid > 0 and nums[mid-1] == target:
+                return 'left'
+            return 'found'
+        elif nums[mid] < target:
+            return 'right'
+        else:
+            return 'left'
+    return binary_search(0, len(nums)-1, condition)
+def last_position(nums, target):
+    def condition(mid):
+        if nums[mid] == target:
+            if mid <len(nums)-1 and nums[mid+1] == target:
+                return 'right'
+            return 'found'
+        elif nums[mid] < target:
+            return 'right'
+        else:
+            return 'left'
+    return binary_search(0, len(nums)-1, condition)
+def binary_search(lo, hi, condition):
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        result = condition(mid)
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid - 1
+        else:
+            lo = mid + 1
+    return -1
