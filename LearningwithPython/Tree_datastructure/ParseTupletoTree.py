df479a790cc854e3b899493b54f49cbb6b5c330c
AV55CS <121622662+AV55CS@users.noreply.github.com>
2023-03-14 22:35:16 +0530

    Update ParseTupletoTree.py
    parsing the tuple to treenode

--- LearningwithPython/Tree_datastructure/ParseTupletoTree.py
+++ LearningwithPython/Tree_datastructure/ParseTupletoTree.py
@@ -21,10 +21,10 @@ def parse_tuple(data):
     else:
         node = TreeNode(data)#if single node (leaf)			
     return node								
-tree2 = parse_tuple(((1,3,None), 2, ((None, 3, 4), 5, (6, 7, 8))))
-tree3 = parse_tuple(((None,4,None), 3, (None, 5, None)))
+tree1 = parse_tuple(((1,3,None), 2, ((None, 3, 4), 5, (6, 7, 8))))
+tree2 = parse_tuple(((None,4,None), 3, (None, 5, None)))
 #we can verify our tree with various print statements
 print(tree2.key)
 #output:3
-print(tree2.left.left.key, tree2.left.right, tree2.right.left.key, tree2.right.right.key)
+print(tree1.left.left.key, tree1.left.right, tree1.right.left.key, tree1.right.right.key)
 #output:(1, None, 3, 7)
