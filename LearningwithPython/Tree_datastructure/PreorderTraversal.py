0b7655a2cddce583bad8df235faff0d4708f9405
AV55CS <121622662+AV55CS@users.noreply.github.com>
2023-03-14 22:54:18 +0530

    Update PreorderTraversal.py

--- LearningwithPython/Tree_datastructure/PreorderTraversal.py
+++ LearningwithPython/Tree_datastructure/PreorderTraversal.py
@@ -1,1 +1,12 @@
+'''
+TreeNode:class for creating and initialzing tree nodes
+traverse_pre_order(node):take root as parameter and return the list of visited keys 
+parse_tuple(data):reads tree tuple and returns root node of the created tree
+tree=root node of the created tree.
 
+Preorder traversal
+    1. Traverse the current node. 
+    2. Traverse the left subtree recursively preorder. 
+    3. Traverse the right subtree recursively preorder. 
+
+'''
