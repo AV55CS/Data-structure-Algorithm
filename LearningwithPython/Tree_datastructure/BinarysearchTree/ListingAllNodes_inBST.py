1abb24d36bd39d429ac33462cccde8bb68bf4a35
AV55CS <121622662+AV55CS@users.noreply.github.com>
2023-03-15 14:36:53 +0530

    Update ListingAllNodes_inBST.py

--- LearningwithPython/Tree_datastructure/BinarysearchTree/ListingAllNodes_inBST.py
+++ LearningwithPython/Tree_datastructure/BinarysearchTree/ListingAllNodes_inBST.py
@@ -1,1 +1,10 @@
+'''
+List the nodes:
+
+function to retrieve all the key-values pairs stored in a BST in the sorted order of keys.
+The nodes can be listed in sorted order by performing an inorder traversal of the BST.
+
+
+Time complexity ="O(N)" and space complexity ="O(N)"
+'''
 
