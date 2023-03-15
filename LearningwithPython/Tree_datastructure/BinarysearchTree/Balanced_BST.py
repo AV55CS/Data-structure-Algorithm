1f0d3a8ea969141a7c4b203ac0b24a3b0dd15ce4
AV55CS <121622662+AV55CS@users.noreply.github.com>
2023-03-15 15:23:41 +0530

    Update Balanced_BST.py
    BstNode,Class User,Objects Listof obejects,make_balanced_bst

--- LearningwithPython/Tree_datastructure/BinarysearchTree/Balanced_BST.py
+++ LearningwithPython/Tree_datastructure/BinarysearchTree/Balanced_BST.py
@@ -1,1 +1,52 @@
+'''
+Recursive strategy
+    • Turning the middle element of the list into the root,
+    • recursively creating left and right subtrees.
+'''
+class BSTNode():
+    def __init__(self, key, value=None):
+        self.key = key
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None 
+        
+class User:
+    def __init__(self, username, name, email):			
+        self.username = username
+        self.name = name
+        self.email = email   
+    def __repr__(self):
+        return "User(username='{}', name='{}', email='{}')".format(self.username, self.name, self.email)
+    def __str__(self):
+        return self.__repr__()
 
+      
+#creating object of user class
+aakash = User('aakash', 'Aakash Rai', 'aakash@example.com')
+biraj = User('biraj', 'Biraj Das', 'biraj@example.com')
+hemanth = User('hemanth', 'Hemanth Jain', 'hemanth@example.com')
+jadhesh = User('jadhesh', 'Jadhesh Verma', 'jadhesh@example.com')
+siddhant = User('siddhant', 'Siddhant Sinha', 'siddhant@example.com')
+sonaksh = User('sonaksh', 'Sonaksh Kumar', 'sonaksh@example.com')
+vishal = User('vishal', 'Vishal Goel', 'vishal@example.com')
+users=[aakash,biraj,hemanth,jadhesh,siddhant,sonaksh,vishal]
+data = [(user.username, user) for user in users]
+
+def make_balanced_bst(data, lo=0, hi=None, parent=None):
+    if hi is None:
+        hi = len(data) - 1
+    if lo > hi:
+        return None							
+    
+    mid = (lo + hi) // 2
+    key, value = data[mid]
+
+    root = BSTNode(key, value)
+    root.parent = parent
+    root.left = make_balanced_bst(data, lo, mid-1, root)
+    root.right = make_balanced_bst(data, mid+1, hi, root)
+    
+    return root
+tree=make_balanced_bst(data)
+print(tree.left.parent.key)
